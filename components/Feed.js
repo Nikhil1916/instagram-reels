eb15f2c3ecbfe0ad6488e4311ef901319c1bdfa2
Nikhil Chawla <nikhil1916.cse18@chitkara.edu.in>
2022-11-08 23:00:08 +0530

    removed consoles

--- components/Feed.js
+++ components/Feed.js
@@ -32,7 +32,6 @@ export default function Feed() {
   const callback = (entries) => {
     entries.forEach((entry) => {
       let ele = entry.target.childNodes[0];
-      // console.log(ele);
       ele.play().then(() => {
         if (!ele.paused && !entry.isIntersecting) {
           ele.pause()
@@ -49,7 +48,6 @@ export default function Feed() {
     const elements = document.querySelectorAll(".video-container");
     let postContainer = elements[0].childNodes;
     postContainer.forEach((video) => {
-      // console.log(video.childNodes[0]); //video tag
       observer.observe(video)
     })
     return () => {
